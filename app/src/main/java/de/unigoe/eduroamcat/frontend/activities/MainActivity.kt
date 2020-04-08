104fa8c97da7b19e528f0cf6eb542aedd0531ca4
angerstoner <angerstoner@mailbox.org>
2020-04-09 00:28:19 +0200

    change gradle version to 6.3; add packages for structuring

--- app/src/main/java/de/unigoe/eduroamcat/frontend/activities/MainActivity.kt
+++ app/src/main/java/de/unigoe/eduroamcat/frontend/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package de.unigoe.eduroamcat
+package de.unigoe.eduroamcat.frontend.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import de.unigoe.eduroamcat.R
 
 class MainActivity : AppCompatActivity() {
 
