87a8e8c3b62217271521b36599bc9cecf21ff086
angerstoner <angerstoner@mailbox.org>
2020-05-10 23:56:49 +0200

    change to DOM-xml parser (org.w3c.dom)

--- app/src/main/java/de/unigoe/eduroamcat/backend/models/EapConfigParser.kt
+++ app/src/main/java/de/unigoe/eduroamcat/backend/models/EapConfigParser.kt
@@ -1,9 +1,12 @@
 package de.unigoe.eduroamcat.backend.models
 
+import android.icu.util.RangeValueIterator
 import android.util.Log
 import android.util.Xml
+import org.w3c.dom.Element
 import org.xmlpull.v1.XmlPullParser
 import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
 
 const val AUTHENTICATION_METHOD = "AuthenticationMethod"
 const val OUTER_EAP_METHOD = "EAPMethod"
@@ -26,88 +29,18 @@ class EapConfigParser {
         private val ns: String? = null
         private val parsedConfig = EapConfig()
 
-        fun parseXml(eapConfigFile: String) {
-            val parser = Xml.newPullParser()
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false)
-            parser.setInput(File(eapConfigFile).inputStream().reader())
-            parser.nextTag()
+        fun parseXml(eapConfigFilePath: String) {
+            val eapConfigFile = File(eapConfigFilePath)
+            val configBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+            val parsedConfigFile = configBuilder.parse(eapConfigFile)
 
-
-            while (parser.next() != XmlPullParser.END_DOCUMENT) {
-                if (parser.eventType == XmlPullParser.START_TAG) {
-                    when (parser.name) {
-                        AUTHENTICATION_METHOD -> readAuthenticationMethod(parser)
-                        CREDENTIAL_APPLICABILITY -> readCredentialApplicability(parser)
-
-                    }
-                }
-            }
-        }
-
-        private fun readAuthenticationMethod(parser: XmlPullParser) {
-            parser.require(XmlPullParser.START_TAG, ns, "AuthenticationMethod")
-            parser.next()
-            while (parser.eventType != XmlPullParser.END_TAG) {
-                when (parser.name) {
-                    OUTER_EAP_METHOD -> readOuterEapMethod(parser)
-                    SERVER_SIDE_CREDENTIALS -> readServerSideCredentials(parser)
-                    CLIENT_SIDE_CREDENTIALS -> readClientSideCredentials(parser)
-                    INNER_AUTHENTICATION_METHOD -> readInnerAuthentication(parser)
-                    else -> Log.i(tag, parser.name)
-                }
-            }
-            Log.i(tag, parsedConfig.toString())
-        }
-
-        private fun readCredentialApplicability(parser: XmlPullParser?) {
-
-        }
-
-        private fun readOuterEapMethod(parser: XmlPullParser) {
-            parser.require(XmlPullParser.START_TAG, ns, OUTER_EAP_METHOD)
-            parser.next()
-            parser.require(XmlPullParser.START_TAG, ns, OUTER_EAP_METHOD_TYPE)
-            parsedConfig.outerEapMethod = readText(parser)
-            parser.next()
-            parser.require(XmlPullParser.END_TAG, ns, OUTER_EAP_METHOD)
-            parser.next()
-        }
-
-        private fun readServerSideCredentials(parser: XmlPullParser) {
-            parser.require(XmlPullParser.START_TAG, ns, SERVER_SIDE_CREDENTIALS)
-            parser.next()
-            parser.require(XmlPullParser.START_TAG, ns, SERVER_SIDE_CERTIFICATE)
-            parsedConfig.serverCertificate = readText(parser)
-            while (parser.name == null || parser.name == SERVER_SIDE_CERTIFICATE) parser.next()
-            parser.require(XmlPullParser.START_TAG, ns, SERVER_ID)
-            parsedConfig.serverId = readText(parser)
-            parser.next()
-            parser.require(XmlPullParser.END_TAG, ns, SERVER_SIDE_CREDENTIALS)
-            parser.next()
-        }
-
-
-        private fun readClientSideCredentials(parser: XmlPullParser) {
-            parser.require(XmlPullParser.START_TAG, ns, CLIENT_SIDE_CREDENTIALS)
-            parser.next()
-            parser.require(XmlPullParser.START_TAG, ns, CLIENT_OUTER_IDENTITY)
-            parsedConfig.outerIdentity = readText(parser)
-            parser.next()
-            parser.require(XmlPullParser.END_TAG, ns, CLIENT_SIDE_CREDENTIALS)
-            parser.next()
-        }
-
-        private fun readInnerAuthentication(parser: XmlPullParser) {
-            parser.next()
-        }
-
-        private fun readText(parser: XmlPullParser): String {
-            var result = ""
-            if (parser.next() == XmlPullParser.TEXT) {
-                result = parser.text
-                parser.nextTag()
-            }
-            return result
+            //TODO: refactor, this was only a test
+            val test = ((parsedConfigFile
+                .getElementsByTagName(AUTHENTICATION_METHOD).item(0) as Element)
+                .getElementsByTagName(OUTER_EAP_METHOD).item(0) as Element)
+                .getElementsByTagName(OUTER_EAP_METHOD_TYPE).item(0).textContent
+//                .getAttribute(OUTER_EAP_METHOD_TYPE)
+            Log.d(tag, test)
         }
     }
 }
