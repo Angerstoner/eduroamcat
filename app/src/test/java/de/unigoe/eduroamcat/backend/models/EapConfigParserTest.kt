a7933154483c6f1e45f5a9dd63858496d7633d57
angerstoner <angerstoner@mailbox.org>
2020-06-30 16:43:30 +0200

    add junit testing to github ci with one test failing on purpose

--- app/src/test/java/de/unigoe/eduroamcat/backend/models/EapConfigParserTest.kt
+++ app/src/test/java/de/unigoe/eduroamcat/backend/models/EapConfigParserTest.kt
@@ -3,11 +3,13 @@ package de.unigoe.eduroamcat.backend.models
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
+
 internal class EapConfigParserTest {
     private val eapConfigParser = EapConfigParser("src/test/res/XmlParserTestConfig.eap-config")
 
     @Test
     fun providerNameTest() {
         assertEquals("GWDG Goettingen - GWDG", eapConfigParser.getProviderName())
+        assertEquals("GWDG Goettinen - GWDG", eapConfigParser.getProviderName())
     }
 }
